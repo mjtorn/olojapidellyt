6d99ea3cbb3b4c58834a3367171e4ce8efdb94e2
Markus Törnqvist <mjt@fadconsulting.com>
2011-12-05 21:15:11 +0200

    Something of a profile

--- olojapidellyt/web/models.py
+++ olojapidellyt/web/models.py
@@ -1,3 +1,28 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.contrib.auth import models as auth_models
+
+from annoying.decorators import signals
+
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    """Basic user profile stuff
+    """
+
+    user = models.OneToOneField(auth_models.User)
+
+    description = models.TextField()
+
+    def __unicode__(self):
+        return u'%s' % self.user
+
+@signals.post_save(sender=auth_models.User)
+def create_profile(instance, **kwargs):
+    if kwargs['created']:
+        profiel = UserProfile.objects.create(user=instance)
+
+# EOF
+
