3c4de11a0285c662e8b3c144b84a573ded2e9a5b
Markus Törnqvist <mjt@fadconsulting.com>
2011-12-05 21:15:26 +0200

    Install web app with userprofile

--- olojapidellyt/settings.py
+++ olojapidellyt/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'olojapidellyt.web'
 )
 
 # A sample logging configuration. The only tangible logging
@@ -146,6 +147,8 @@ LOGGING = {
         },
     }
 }
+
+AUTH_PROFILE_MODULE = 'web.UserProfile'
 
 try:
     from custom_settings import *
