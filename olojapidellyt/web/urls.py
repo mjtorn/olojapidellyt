b5e118d96d9dbada9f14b8d0f3665407e1d550d8
Markus Törnqvist <mjt@fadconsulting.com>
2011-12-06 01:47:46 +0200

    logout url

--- olojapidellyt/web/urls.py
+++ olojapidellyt/web/urls.py
@@ -1,6 +1,8 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
 from django.conf.urls.defaults import *
+from django.contrib.auth import views as auth_views
+
 from dagny.urls import resources
 
 from olojapidellyt.web import views
@@ -8,6 +10,7 @@ from olojapidellyt.web import views
 urlpatterns = patterns('',
     url(r'^tarinat/', resources('web.resources.Story', name='Story', id=('slug', r'[\w\-\d]+'))),
     url(r'^profiili/', resources('web.resources.UserProfile', name='UserProfile', id=('username', r'[\w\-_@\d]+'))),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^$', views.index, name='index'),
 )
 
