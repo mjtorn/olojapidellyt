2161a01030b6f8c2ca7edc9a41eb2801a766c416
Markus Törnqvist <mjt@fadconsulting.com>
2011-12-05 23:22:38 +0200

    Google analytics ctxproc

--- olojapidellyt/context_processors.py
+++ olojapidellyt/context_processors.py
@@ -2,9 +2,16 @@
 
 from django.contrib.sites import models as sites_models
 
+from django.conf import settings
+
 def site(request):
     return {
         'site': sites_models.Site.objects.get_current(),
+    }
+
+def analytics(request):
+    return {
+        'GA_ACCOUNT_NUM': getattr(settings, 'GA_ACCOUNT_NUM', None) if not settings.DEBUG else None
     }
 
 # EOF
