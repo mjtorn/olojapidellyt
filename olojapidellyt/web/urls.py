488cfdb0165874d9ed9cb9139cfcf765fee12094
Markus Törnqvist <mjt@fadconsulting.com>
2011-12-05 22:03:49 +0200

    index

--- olojapidellyt/web/urls.py
+++ olojapidellyt/web/urls.py
@@ -3,8 +3,11 @@
 from django.conf.urls.defaults import *
 from dagny.urls import resources
 
+from olojapidellyt.web import views
+
 urlpatterns = patterns('',
     url(r'^tarinat/', resources('web.resources.Story', name='Story', id=('slug', r'[\w\-\d]+'))),
+    url(r'^$', views.index, name='index'),
 )
 
 # EOF
--- olojapidellyt/web/views.py
+++ olojapidellyt/web/views.py
@@ -1,1 +1,24 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.shortcuts import render_to_response
+
+from django.template import RequestContext
+
+from olojapidellyt.web import models
+
 # Create your views here.
+
+def index(request):
+    """Probably the only view in town
+    """
+
+    recent = models.Story.objects.get_visible()
+    recent = recent.order_by('-posted_at')
+    recent = recent[:5]
+
+    req_ctx = RequestContext(request, locals())
+
+    return render_to_response('index.html', req_ctx)
+
+# EOF
+
