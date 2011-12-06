b40350fcd440760d775788a128ff0d08d324bb03
Markus Törnqvist <mjt@fadconsulting.com>
2011-12-06 13:01:35 +0200

    Manager methods

--- olojapidellyt/web/managers.py
+++ olojapidellyt/web/managers.py
@@ -12,5 +12,35 @@ class StoryManager(models.Manager):
 
         return self.select_related('user').filter(visible=True)
 
+    def get_recent(self):
+        """Recent stories
+        """
+
+        recent = self.get_visible().order_by('-posted_at')
+        recent = recent[:5]
+
+        return recent
+
+    def get_top_moods(self):
+        """Top moods
+        """
+
+        top_moods = self.get_visible().order_by('-posted_at', '-mood')
+        top_moods = top_moods[:5]
+
+        return top_moods
+
+    def get_random_story(self):
+        """Returns None if nothing found
+        """
+
+        random_story = self.get_visible().order_by('?')
+        try:
+            random_story = random_story[0]
+        except IndexError:
+            random_story = None
+
+        return random_story
+
 # EOF
 
