82897d7690b6fac11ca4da3d6ef60f40da03f02e
Markus Törnqvist <mjt@fadconsulting.com>
2011-12-06 02:18:00 +0200

    Terms

--- olojapidellyt/web/urls.py
+++ olojapidellyt/web/urls.py
@@ -1,5 +1,7 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
+from django.views.generic.simple import direct_to_template
+
 from django.conf.urls.defaults import *
 from django.contrib.auth import views as auth_views
 
@@ -12,6 +14,9 @@ urlpatterns = patterns('',
     url(r'^profiili/', resources('web.resources.UserProfile', name='UserProfile', id=('username', r'[\w\-_@\d]+'))),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^poista/$', views.clear, name='clear'),
+    url(r'^ehdot/$', direct_to_template, {
+        'template': 'terms.html',
+    }, name='terms'),
     url(r'^$', views.index, name='index'),
 )
 
